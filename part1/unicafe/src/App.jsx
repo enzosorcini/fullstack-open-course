1a155fcb41b933d705c7be7bdc2129de7cba9214
enzosorcini <enzosorcini.w@gmail.com>
2024-04-15 22:21:13 +0200

    finished 1.6 | check for refactor to best practices

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,5 +1,18 @@
 import { useState } from 'react'
 
+const Button = ({ handleClick, text}) => <button onClick={handleClick}>{text}</button>
+
+const Display = ({ good, neutral, bad}) => {
+  return (
+    <div>
+      <p>Good {good}</p>
+      <p>Neutral {neutral}</p>
+      <p>Bad {bad}</p>
+    </div>
+  )
+}
+
+//REFACTOR TO BEST PRACTICES
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -7,19 +20,18 @@ const App = () => {
   const [bad, setBad] = useState(0)
 
   //refactor names to best practices
-  const goodFeedback = () => {}
-  const neutralFeedback = () => {}
-  const badFeedback = () => {}
-
-  const Button = ({ doSomething, text}) => <button onClick={doSomething}>{text}</button>
+  const giveGoodFeedback = () => setGood(good + 1)
+  const giveNeutralFeedback = () => setNeutral(neutral + 1)
+  const giveBadFeedback = () => setBad(bad + 1)
 
   return (
     <div>
       <h1>Give Feedback</h1>
-      <Button doSomething={goodFeedback} text='Good'/>
-      <Button doSomething={neutralFeedback} text='Neutral'/>
-      <Button doSomething={badFeedback} text='Bad'/>
+      <Button handleClick={giveGoodFeedback} text='Good'/>
+      <Button handleClick={giveNeutralFeedback} text='Neutral'/>
+      <Button handleClick={giveBadFeedback} text='Bad'/>
       <h2>Statistics</h2>
+      <Display good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
