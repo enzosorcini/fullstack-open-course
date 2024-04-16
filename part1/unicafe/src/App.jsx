e06d7c0172d0fabb501630f3a8420bc8c5a5cce5
enzosorcini <enzosorcini.w@gmail.com>
2024-04-16 23:00:59 +0200

    finished 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -3,23 +3,32 @@ import { useState } from 'react'
 const Button = ({ handleClick, text}) => <button onClick={handleClick}>{text}</button>
 
 const Display = ({ good, neutral, bad}) => {
+  let totalSum = good + neutral + bad
+  let average = (good - bad)/totalSum
+  let positivePercentage = (good/totalSum) * 100
+
+  console.log('Total sum:', totalSum)
+  console.log('Average:', average)
+  console.log('Percentage of + feedback:', positivePercentage)
+
   return (
     <div>
       <p>Good {good}</p>
       <p>Neutral {neutral}</p>
       <p>Bad {bad}</p>
+      <p>All {totalSum}</p>
+      <p>Average {average}</p>
+      <p>Positive {positivePercentage} %</p>
     </div>
   )
 }
 
-//REFACTOR TO BEST PRACTICES
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  //refactor names to best practices
   const giveGoodFeedback = () => setGood(good + 1)
   const giveNeutralFeedback = () => setNeutral(neutral + 1)
   const giveBadFeedback = () => setBad(bad + 1)
