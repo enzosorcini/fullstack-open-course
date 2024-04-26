6424fab291de5ba67d8dc223bf4f3b8530859af7
enzosorcini <enzosorcini.w@gmail.com>
2024-04-26 21:37:09 +0200

    finished 2.5 | courseinfo step 10

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,54 +1,55 @@
 import Course from './components/Course'
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
-/*
-  const course2 = {
-    id: 2,
-    name: 'Half Stack application development II',
-    parts: [
-      {
-        name: 'Fundamentals of React II',
-        exercises: 15,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data II',
-        exercises: 5,
-        id: 2
-      },
-      {
-        name: 'State of a component II',
-        exercises: 8,
-        id: 3
-      }
-    ]
-  }
-*/
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
   return (
     <div>
-      <Course key={course.id} course={course} />
-      {/*<Course key={course2.id} course={course2} />*/}
+      <h1>Web Development Curriculum</h1>
+      {courses.map( course => <Course key={course.id} course={course} /> )}
     </div>
   )
 }
--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,18 +1,13 @@
-const Header = ({ text }) => <h1>{text}</h1>
+const Header = ({ text }) => <h2>{text}</h2>
 
-const Part = ({ part }) => {
-  console.log('part inside Part', part);
-  return <li>{part.name} {part.exercises}</li>
-}
+const Part = ({ part }) => <li>{part.name} {part.exercises}</li>
 
 const Content = ({ parts }) => {
   console.log('parts sent to Content',parts);
   return (
-    <div>
-      <ul>
-        {parts.map( part => <Part key={part.id} part={part}/>)}
-      </ul>
-    </div>
+    <ul>
+      {parts.map( part => <Part key={part.id} part={part}/>)}
+    </ul>
   )
 }
 
@@ -20,18 +15,13 @@ const TotalExercises = ({ parts }) => {
   let totalExercises = parts.reduce((sum, part) => {
     return sum + part.exercises
   }, 0)
-  
+
   console.log('Total sum', totalExercises);
 
-  return (
-    <div>
-      <p>Total of {totalExercises} exercises</p>
-    </div>
-  )
+  return <h4>Total of {totalExercises} exercises</h4>
 }
 
 const Course = ({ course }) => {
-  console.log('course sent to Course', course);
   return (
     <div>
       <Header text={course.name}/>
