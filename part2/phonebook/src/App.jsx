c9e40c201d9201dd30937c4404161e637b77e952
enzosorcini <enzosorcini.w@gmail.com>
2024-05-01 10:59:19 +0200

    finished 2.9 | phonebook step 4

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,16 +2,17 @@ import { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '123123123' }
+    { name: 'Arto Hellas', number: '123123123', id: 1}
   ]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ filter, setFilter ] = useState('')
   
   const addPerson = (event) => {
     event.preventDefault()
-    const newPerson = { name: newName, number: newNumber}
+    const newPerson = { name: newName, number: newNumber, id: persons.length + 1}
 
-    const notExistsInPhonebook = persons.every( person => JSON.stringify(person) !== JSON.stringify(newPerson))
+    const notExistsInPhonebook = persons.every( person => JSON.stringify(person.name).toLowerCase() !== JSON.stringify(newPerson.name).toLowerCase())
     console.log('after function', notExistsInPhonebook);
 
     if(notExistsInPhonebook){
@@ -29,9 +30,23 @@ const App = () => {
 
   const handleNumberChange = numberEvent => setNewNumber(numberEvent.target.value)
 
+  const handleFilterChange = filterEvent => setFilter(filterEvent.target.value)
+
+  let filteredPhonebook;
+  console.log('filter status', filter);
+  if (filter === '') {
+    console.log('empty filter', filter);
+    filteredPhonebook = persons
+  }else{
+    console.log('filter active', filter);
+    filteredPhonebook = persons.filter( person => person.name.toLowerCase().includes(filter.toLowerCase()))
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>filter names with: <input value={filter} onChange={handleFilterChange}/></div>
+      <h2>Add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange}/>
@@ -45,7 +60,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        { persons.map( person => <li key={person.name}>{person.name} {person.number}</li>)}
+        { filteredPhonebook.map( person => <li key={person.id}>{person.name} {person.number}</li>)}
       </ul>
     </div>
   )
