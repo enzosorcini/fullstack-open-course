0fb8e3224d494c87b6af9ae968e6315e4f8fad2a
enzosorcini <enzosorcini.w@gmail.com>
2024-05-11 12:11:51 +0200

    finished 2.17 | phonebook step 12

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -5,6 +5,7 @@ import Phonebook from './components/PhoneBook'
 import PersonForm from './components/PersonForm'
 import Filter from './components/Filter'
 import Notification from './components/Notification'
+import Error from './components/Error'
 
 const App = () => {
   const [ persons, setPersons ] = useState([]) 
@@ -12,6 +13,7 @@ const App = () => {
   const [ newNumber, setNewNumber ] = useState('')
   const [ filter, setFilter ] = useState('')
   const [ notification, setNotification ] = useState(null)
+  const [ error, setError ] = useState(null)
   
   useEffect( () => {
     console.log('first render')
@@ -41,7 +43,11 @@ const App = () => {
           setNotification(`Added ${returnedNewPerson.name} to the phonebook`)
           setTimeout(() => {setNotification(null)}, 5000)
         })
-        .catch( error => console.log('error creating new person', error))
+        .catch( error => {
+          console.log('error creating new person', error)
+          setError(`Cannot add ${returnedNewPerson.name} to the phonebook`)
+          setTimeout(() => {setError(null)}, 5000)
+        })
     }else{
       console.log('person already exists', newPerson);
       if(window.confirm(`${newName} is already added to the phonebook, replace the old number with a new one?`)){
@@ -58,15 +64,14 @@ const App = () => {
             setNotification(`Changed ${updatedPerson.name}'s number to ${updatedPerson.number}`)
             setTimeout(() => {setNotification(null)}, 5000)
           })
+          .catch( error => {
+            console.log('error updating person', error)
+            setError(`Information of ${personInPhonebook.name} was already removed from the server`)
+            setTimeout(() => {setError(null)}, 5000)
+          })
       }
     }
   }
-
-  const handleNameChange = nameEvent => setNewName(nameEvent.target.value)
-
-  const handleNumberChange = numberEvent => setNewNumber(numberEvent.target.value)
-
-  const handleFilterChange = filterEvent => setFilter(filterEvent.target.value)
 
   const handleDelete = id => {
     console.log('delete person', id)
@@ -81,9 +86,19 @@ const App = () => {
           setNotification(`Deleted ${deletedPerson.name} from the phonebook`)
           setTimeout(() => {setNotification(null)}, 5000)
         })
-        .catch( error => console.log('error deleting person', error))
+        .catch( error => {
+          console.log('error deleting person', error)
+          setError(`Cannot delete ${personToDelete.name} from the phonebook`)
+          setTimeout(() => {setError(null)}, 5000)
+        })
     }
   }
+  
+  const handleNameChange = nameEvent => setNewName(nameEvent.target.value)
+
+  const handleNumberChange = numberEvent => setNewNumber(numberEvent.target.value)
+
+  const handleFilterChange = filterEvent => setFilter(filterEvent.target.value)
   
   let filteredPhonebook;
   if (filter === '') {
@@ -96,6 +111,7 @@ const App = () => {
     <div>
       <h2>Phonebook</h2>
       <Notification message={notification}/>
+      <Error message={error}/>
       <Filter filterState={filter} filterHandler={handleFilterChange}/>
       <h3>Add a new</h3>
       <PersonForm
